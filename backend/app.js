6f04d40d93756f1eeea7fe5a9797f82b4588b3ed
Nikita Valitov <valitov.nekit@yandex.ru>
2023-11-29 15:00:18 +0300

    fix

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { DB_ADDRESS } = process.env;
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 
 const app = express();
 app.use(cors());
